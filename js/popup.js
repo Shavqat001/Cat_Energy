78ced8c155474d819911e05490d47e6f7f2ebab2
Shavqqat001 <nazriev.shavqatjon@gmail.com>
2023-04-19 16:34:24 +0500

    doing adaptive block header

--- js/popup.js
+++
@@ -1,6 +0,0 @@
-
-let name="ман "
-let number=5
-let name2="-то ролтон хурдам "
-let result=name+number+name2;
-console.log(result);
---
+++ js/toggle.js
@@ -0,0 +1,5 @@
+let toggleBtn = document.querySelector('.page-header__toggle');
+let mainNav = document.querySelector('.main-nav');
+toggleBtn.addEventListener('click', () => {
+  mainNav.classList.toggle('visually-hidden');
+});
