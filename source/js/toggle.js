8f62ae589011911c80c1b279008af70fc7b0abbb
Shavqqat001 <nazriev.shavqatjon@gmail.com>
2023-05-01 23:47:02 +0500

    some changes on form page

--- source/js/toggle.js
+++ source/js/toggle.js
@@ -2,5 +2,5 @@ let toggleBtn = document.querySelector('.page-header__toggle');
 let mainNav = document.querySelector('.main-nav');
 
 toggleBtn.addEventListener('click', () => {
-  mainNav.classList.toggle('visually-hidden');
+  mainNav.classList.toggle('.closed');
 });
